1c9d45eeb6ae19e669dc29a9ec6d1663fb6f926a
Jmoney <67088606+AsanroJay@users.noreply.github.com>
2025-11-29 23:16:34 +0800

    feat: update card class logic

--- tadhana--baraha-ng-huling-babaylan-/cards/card.gd
+++ tadhana--baraha-ng-huling-babaylan-/cards/card.gd
@@ -1,15 +1,55 @@
 extends Node
-class_name Card
+class_name CardDB
 
-var card_name: String
-var cost: int
-var image_path: String
-var description: String
-var type: String
+# Master dictionary of all cards
+var CARDS := {
+	"default": {
+		"card_name": "Default",
+		"type": "attack",
+		"cost": 1,
+		"image_path": "res://cards/assets/default_card.png",
+		"damage": 6,
+		"block": 0,
+		"effect": null
+	},
 
-func _init(n , c , img_path , desc, t):
-	card_name = n
-	cost = c
-	image_path = img_path
-	description = desc
-	type = t
+	"defend": {
+		"card_name": "Defend",
+		"type": "defend",
+		"cost": 1,
+		"image_path": "res://cards/assets/defend.png",
+		"damage": 0,
+		"block": 5,
+		"effect": null
+	},
+
+	"strike": {
+		"card_name": "Strike",
+		"type": "attack",
+		"cost": 1,
+		"image_path": "res://cards/assets/strike.png",
+		"damage": 6,
+		"block": 0,
+		"effect": null
+	},
+
+	"bash": {
+		"card_name": "Bash",
+		"type": "attack",
+		"cost": 2,
+		"image_path": "res://cards/assets/bash.png",
+		"damage": 8,
+		"block": 0,
+		"effect": "apply_vulnerable"
+	}
+}
+
+
+# Returns a clean COPY of card data (NOT a reference)
+func get_card(id: String) -> Dictionary:
+	if CARDS.has(id):
+		return CARDS[id].duplicate(true)
+	else:
+		push_warning("Card ID not found: %s" % id)
+		return CARDS["default"].duplicate(true)
+		
